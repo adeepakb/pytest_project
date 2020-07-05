a93db0ee7f364ce86c244e32b800390abbc963e3
Reshma R Nair <reshma.nair@byjus.com>
2020-07-05 23:25:51 +0530

    updated pre-execution file

--- src/Utilities/pre_execution.py
+++ src/Utilities/pre_execution.py
@@ -10,46 +10,47 @@ baseClass = BaseClass()
 
 
 class BuildFeatureJob():
-    j = jenkins.Jenkins('https://builds.byjus.com/', username='testautomation@byjus.com',
-                        password='5a7b8f02aec0c2a7c5d4c348a1c7a590')
-    # branch_parameters = dict(branch='origin/feature/app2_phase3', env='staging', variant='ByjusPremium')
-    # j.build_job('B2C-Feature', parameters=branch_parameters, token='5a7b8f02aec0c2a7c5d4c348a1c7a590')
-    # time.sleep(10)
-    # while True:
-    #     print('Running....')
-    #     if j.get_job_info('B2C-Feature')['lastCompletedBuild']['number'] == j.get_job_info('B2C-Feature')['lastBuild'][
-    #         'number']:
-    #         print("Last ID %s, Current ID %s" % (j.get_job_info('B2C-Feature')['lastCompletedBuild']['number'],
-    #                                              j.get_job_info('B2C-Feature')['lastBuild']['number']))
-    #         break
-    # time.sleep(3)
-    # print('Stop....')
-    # last_build_number = j.get_job_info('B2C-Feature')['lastCompletedBuild']['number']
-    last_build_number = 1694
-    print("last_build_number", last_build_number)
-    build_info = j.get_build_info('B2C-Feature', last_build_number)
+    def __init__(self):
+        j = jenkins.Jenkins('https://builds.byjus.com/', username='testautomation@byjus.com',
+                            password='5a7b8f02aec0c2a7c5d4c348a1c7a590')
+        # branch_parameters = dict(branch='origin/feature/app2_phase3', env='staging', variant='ByjusPremium')
+        # j.build_job('B2C-Feature', parameters=branch_parameters, token='5a7b8f02aec0c2a7c5d4c348a1c7a590')
+        # time.sleep(10)
+        # while True:
+        #     print('Running....')
+        #     if j.get_job_info('B2C-Feature')['lastCompletedBuild']['number'] == j.get_job_info('B2C-Feature')['lastBuild'][
+        #         'number']:
+        #         print("Last ID %s, Current ID %s" % (j.get_job_info('B2C-Feature')['lastCompletedBuild']['number'],
+        #                                              j.get_job_info('B2C-Feature')['lastBuild']['number']))
+        #         break
+        # time.sleep(3)
+        # print('Stop....')
+        # last_build_number = j.get_job_info('B2C-Feature')['lastCompletedBuild']['number']
+        last_build_number = 1694
+        print("last_build_number", last_build_number)
+        build_info = j.get_build_info('B2C-Feature', last_build_number)
 
-    if build_info['result'] == 'SUCCESS':
-        print("Build Success")
-        artifact = build_info['artifacts'][0]
-        artifact_displaypath = artifact['displayPath']
-        apk_url = 'https://builds.byjus.com/job/B2C-Feature/' + str(
-            last_build_number) + '/artifact/app/build/outputs/apk/ByjusPremium/release/' + artifact_displaypath
-        r = requests.Request('GET', apk_url)
-        response = j.jenkins_request(r)
-        with open("app.apk", "wb") as f:
-            f.write(response.content)
+        if build_info['result'] == 'SUCCESS':
+            print("Build Success")
+            artifact = build_info['artifacts'][0]
+            artifact_displaypath = artifact['displayPath']
+            apk_url = 'https://builds.byjus.com/job/B2C-Feature/' + str(
+                last_build_number) + '/artifact/app/build/outputs/apk/ByjusPremium/release/' + artifact_displaypath
+            r = requests.Request('GET', apk_url)
+            response = j.jenkins_request(r)
+            with open("app.apk", "wb") as f:
+                f.write(response.content)
 
-        browser = baseClass.driverSetup()
-        time.sleep(5)
-        subprocess.Popen('adb connect ' + getdata(CONFIG_PATH, 'adb_connect', 'headspin_device'), shell=True,stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
-        val = subprocess.Popen('adb install -r ../../tests/step_def/app.apk', shell=True, stdout=subprocess.PIPE,stderr=subprocess.STDOUT).communicate(timeout=660000)
-        print("adb install status ", val)
-        print(artifact_displaypath+" is installed successfully")
+            browser = baseClass.driverSetup()
+            time.sleep(5)
+            subprocess.Popen('adb connect ' + getdata(CONFIG_PATH, 'adb_connect', 'headspin_device'), shell=True,stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
+            val = subprocess.Popen('adb install -r ../../tests/step_def/app.apk', shell=True, stdout=subprocess.PIPE,stderr=subprocess.STDOUT).communicate(timeout=660000)
+            print("adb install status ", val)
+            print(artifact_displaypath+" is installed successfully")
 
-    else:
-        print("Build Failed")
-        log = j.get_build_console_output('B2C-Feature', last_build_number)
-        f = open('log_buildFail.txt', 'w')
-        f.write(log)
-        f.close()
+        else:
+            print("Build Failed")
+            log = j.get_build_console_output('B2C-Feature', last_build_number)
+            f = open('log_buildFail.txt', 'w')
+            f.write(log)
+            f.close()
