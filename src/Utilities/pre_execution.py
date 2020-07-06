d229bfa87d631650d35f8a9164241180bc5df526
Reshma R Nair <reshma.nair@byjus.com>
2020-07-06 18:09:33 +0530

    updated preexecution file

--- src/Utilities/pre_execution.py
+++ src/Utilities/pre_execution.py
@@ -18,15 +18,16 @@ class BuildFeatureJob():
         # time.sleep(10)
         # while True:
         #     print('Running....')
-        #     if j.get_job_info('B2C-Feature')['lastCompletedBuild']['number'] == j.get_job_info('B2C-Feature')['lastBuild'][
-        #         'number']:
+        #     if j.get_job_info('B2C-Feature')['lastCompletedBuild']['number'] == \
+        #             j.get_job_info('B2C-Feature')['lastBuild'][
+        #                 'number']:
         #         print("Last ID %s, Current ID %s" % (j.get_job_info('B2C-Feature')['lastCompletedBuild']['number'],
         #                                              j.get_job_info('B2C-Feature')['lastBuild']['number']))
         #         break
-        # time.sleep(3)
+        #     time.sleep(10)
         # print('Stop....')
         # last_build_number = j.get_job_info('B2C-Feature')['lastCompletedBuild']['number']
-        last_build_number = 1698
+        last_build_number = 1700
         print("last_build_number", last_build_number)
         build_info = j.get_build_info('B2C-Feature', last_build_number)
 
@@ -40,13 +41,32 @@ class BuildFeatureJob():
             with open("app.apk", "wb") as f:
                 f.write(response.content)
 
-            baseClass.driverSetup()
-            time.sleep(5)
-            subprocess.Popen('adb connect ' + getdata(CONFIG_PATH, 'adb_connect', 'headspin_device'), shell=True,stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
-            time.sleep(2)
-            val = subprocess.Popen('adb install -r ../../tests/step_def/app.apk', shell=True, stdout=subprocess.PIPE,stderr=subprocess.STDOUT).communicate()
-            print("adb install status ", val)
-            print(artifact_displaypath + " is installed successfully")
+            driver = baseClass.driverSetup()
+            connected = False
+            for i in range(4):
+                subprocess.Popen('adb connect ' + getdata(CONFIG_PATH, 'adb_connect', 'headspin_device'), shell=True,
+                                 stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
+                stdout, stderr = subprocess.Popen('adb devices', shell=True, stdout=subprocess.PIPE,
+                                                  stderr=subprocess.STDOUT).communicate()
+                stdout = stdout.decode('ascii')
+                if getdata(CONFIG_PATH, 'adb_connect', 'headspin_device') in stdout and "unauthorized" not in stdout:
+                    print("adb connected successfully")
+                    connected = True
+                    break
+                print("Connection failed, retrying...")
+                time.sleep(1 + i)
+
+            if not connected:
+                raise Exception("Failed to connect to device")
+
+            stdout, stderr = subprocess.Popen('adb install -r ../../tests/step_def/app.apk', shell=True,
+                                              stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
+            print("adb install status ", stdout, stderr)
+            output = stdout.decode("ascii")
+            if "Success" not in output or "1 file pushed, 0 skipped." not in output:
+                raise Exception("Failed to install app due to error %s" % output)
+            print("latest apk installed successfully " + artifact_displaypath)
+            driver.quit()
 
         else:
             print("Build Failed")
