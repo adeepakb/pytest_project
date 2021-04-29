ddb12b1cd372a08955a09b7202d6cfe2fddbff4a
Suguresh Soppimath <suguresh.soppimath@byjus.com>
2021-04-29 11:16:04 +0530

    rename function 'getdata' to 'get_data'

--- src/constants/load_json.py
+++ src/constants/load_json.py
@@ -2,7 +2,7 @@
 import json
 
 
-def getdata(filename, var, key=None):
+def get_data(filename, var, key=None):
     if key is not None:
         with open(filename) as f:
             return json.load(f)[var][key]
