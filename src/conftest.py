7911ea4e521c237ecdb5bb93f6fd07055d5af59c
adeepakb2 <deepak.sharma11@byjus.com>
2021-09-14 21:29:05 +0530

    deepak chat chnages

--- src/conftest.py
+++ src/conftest.py
@@ -1,63 +1,69 @@
 import re
-import traceback
+import time
 import os
-import sys
+import traceback
+import jenkins
 import pytest
+import sys
 import subprocess
 import logging
-
-from selenium.common.exceptions import InvalidSessionIdException
-from utilities.BasePage import BaseClass
-from utilities.common_methods import CommonMethods
-#from utilities.pre_execution import BuildFeatureJob
+from utilities.base_page import BaseClass
+from utilities.pre_execution import BuildFeatureJob
 from constants.test_management import *
-#from constants.loadFeatureFile import fetch_feature_file
-#from utilities.base_page import BaseClass
-
+from constants.loadFeatureFile import fetch_feature_file
 from tests.common_steps import *
 
-base_class = BaseClass()
-# CommonMethods = CommonMethods()
-#feature_job = BuildFeatureJob()
+PATH = lambda p: os.path.abspath(
+    os.path.join(os.path.dirname(__file__), p))
+sys.path.append(PATH('constants/'))
+from constants.test_management import *
+
+baseClass = BaseClass()
+feature_job = BuildFeatureJob()
 
 
-# @pytest.fixture(scope="session", autouse=True)
-# def setup_teardown():
-#     feature_job.build_and_install_apk()
-#     yield
-#     # Create report on demand via  API at the end of the session
-#     suitename = os.getenv('suite')
-#     if suitename == 'Regression_PremiumApp_Automation':
-#         report_id = get_testrail_reports(13, 'Regression Run (Summary) %date%')
-#         run_testrail_reports(report_id)
-#     elif suitename == 'Sanity_PremiumApp_Automation':
-#         report_id = get_testrail_reports(13, 'Sanity Run (Summary) %date%')
-#         run_testrail_reports(report_id)
+@pytest.fixture(scope="session", autouse=True)
+def setup_teardown():
+    # feature_job.build_and_install_apk()
+    job_start_time = time.time()
+    pass
+    yield
+    # Get total execution time
+    job_total_execution_time = str(datetime.timedelta(seconds=int(time.time() - job_start_time)))
+    print(job_total_execution_time)
+    # Update execution time to testrail for android test run. Create report on demand via  API at the end of the session
+    suitename = os.getenv('suite')
+    if suitename == "Byju's Classes":
+        update_run_for_execution_time('503', job_total_execution_time)
+        report_id = get_testrail_reports(24, "Daily Regression automation report For Byju's Classes Android %date%")
+        run_testrail_reports(report_id)
 
 
 def pytest_addoption(parser):
     parser.addoption("--platform", action="append")
 
 
+def capture_screenshot(request, feature_name):
+    driver = request.getfixturevalue("driver")
+    timestamp = datetime.datetime.now().strftime("%d-%m-%y, %H-%M-%S")
+    screenshot_filename = feature_name + " " + timestamp + ".png"
+    driver.get_screenshot_as_file(screenshot_filename)
+    return screenshot_filename
+
 @pytest.fixture()
 def driver(request):
-    platform_list = request.config.getoption("platform")
-    if Platform.ANDROID.name.lower() == platform_list[-1].lower():
-        #print(subprocess.getoutput('adb devices & adb disconnect'))
-        android_driver = base_class.setup_android()
-        # feature_job.lock_or_unlock_device('lock')
-        # serial = feature_job.connect_adb_api()
-        # feature_job.connect_to_adb(serial)
+    platform_list = request.config.getoption("--platform")
+    if Platform.ANDROID.name in platform_list:
+        android_driver = baseClass.setup_android()
+        feature_job.lock_or_unlock_device('lock')
+        serial = feature_job.connect_adb_api()
+        feature_job.connect_to_adb(serial)
         yield android_driver
-        # subprocess.Popen('adb disconnect ' + serial,
-        #                  shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
-        try:
-            android_driver.quit()
-        except InvalidSessionIdException:
-            """mostly socket was already hung up"""
-            raise
-    elif Platform.WEB.name.lower() == platform_list[-1].lower():
-        chrome_driver = base_class.setup_browser()
+        subprocess.Popen('adb disconnect ' + serial, shell=True, stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT).communicate()
+        android_driver.quit()
+    elif Platform.WEB.name in platform_list:
+        chrome_driver = baseClass.setup_browser()
         yield chrome_driver
         chrome_driver.quit()
     else:
@@ -65,6 +71,7 @@ def driver(request):
         raise NotImplementedError(f"'{platform}' is not yet implemented.")
 
 
+# ---------------------------testrail updation--------------------
 def py_test():
     """
     Create and update the local-variable and reuse the same when required.
@@ -81,6 +88,7 @@ def pytest_bdd_before_scenario(feature):
        :returns: None
        """
     py_test.exception = None
+    py_test.start = time.time()
     feature_name = feature.name
     if feature_name == 'Register Screen':
         subprocess.Popen('adb shell pm clear com.byjus.thelearningapp.premium', shell=True)
@@ -118,8 +126,8 @@ def pytest_bdd_step_validation_error(step):
 
 def pytest_bdd_step_error(step):
     """
-     Called when step function failed to execute.
-      Update the value ``True`` of ``py_test.exception`` as exception occurred.
+    Called when step function failed to execute.
+    Update the value ``True`` of ``py_test.exception`` as exception occurred.
     :type step: Step
     :returns: None
     """
@@ -146,9 +154,17 @@ def pytest_bdd_after_scenario(request, feature, scenario):
     prj_path_only = os.path.abspath(os.getcwd() + "/../..")
     feature_name = feature.name
     scenario_name = scenario.name
-    suite_name= "Byju's Classes"
-    # suite_name = os.getenv('suite')
+    elapsed = int(time.time() - py_test.__getattribute__('start'))
+    elapsed_time = str(elapsed) + 's'
+    suite_name = os.getenv('suite')
+    # suite_name = "Byju's Classes"
+    if suite_name == "Byju's Classes":
+        testing_device = request.getfixturevalue("driver").session['deviceModel']
+        app_version = baseClass.get_current_app_version()
+    else:
+        raise NotImplementedError()
     data = get_run_and_case_id_of_a_scenario(suite_name, scenario.name, "24", "199")
+    print("\nTestcase executed: %s" %scenario_name)
     if py_test.__getattribute__("exception") or value:
         trc = re.findall(r'Traceback.*', ''.join(summaries))[-1] + "\n"
         _exception = list(filter(lambda summary:
@@ -161,6 +177,7 @@ def pytest_bdd_after_scenario(request, feature, scenario):
         _exception.insert(0, trc)
         _exception.append(summaries[-1])
         _exception = "".join(_exception)
+        screenshot_filename = capture_screenshot(request,feature_name)
         if not value:
             step_name = py_test.__getattribute__('failed_step_name')
         else:
@@ -174,11 +191,11 @@ def pytest_bdd_after_scenario(request, feature, scenario):
                 "=" * 100 + "Failures" + "=" * 100
         )
         sys.stderr.writelines(stdout_err)
-
-        #update_testrail(data[1], data[0], False, step_name, _exception)
+        update_testrail(data[1], data[0], False, step_name, _exception, elapsed_time, testing_device, app_version)
+        add_attachment_to_result(data[0], data[1], screenshot_filename)
     else:
         msg_body = "all steps are passed"
-        #update_testrail(data[1], data[0], True, msg_body, 'passed')
+        update_testrail(data[1], data[0], True, '', msg_body, elapsed_time, testing_device ,app_version)
     file = '../../config/chrome_session.json'
     try:
         os.unlink(file)
